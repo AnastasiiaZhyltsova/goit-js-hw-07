e0aeee54834ce353428c53baf8c1b443bf692b88
AnastasiiaZhyltsova <anastasiiazhyltsova@gmail.com>
2022-05-21 13:30:57 +0300

    gl_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');   
+
+
+const createGalleryItems = galleryItems.map((galleryItem)=> 
+    `
+         <a class="gallery__item" href="${galleryItem.original}">
+              <img
+              class="gallery__image"
+              src="${galleryItem.preview}" 
+              alt="${galleryItem.description}"
+              data-source="${galleryItem.original}"
+        />
+       </a>
+   `
+).join('');
+
+gallery.insertAdjacentHTML("beforeend", createGalleryItems);
+
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt" , captionDelay: 250});
+
+gallery.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+    event.preventDefault();
+    // console.log(event.target);
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+}
